28e60f825db277765d8d165c318e84d1e55eedd7
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-13 14:35:16 +0200

    art section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Coctails from "./components/Coctails";
 import About from "./components/About";
+import Art from "./components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -14,6 +15,7 @@ const App = () => {
       <Hero />
       <Coctails />
       <About />
+      <Art />
     </main>
   );
 };
